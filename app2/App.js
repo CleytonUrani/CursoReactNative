be448e85871cf262c164cd6f08cfa17c454e0769
Cleyton Artur Soares Urani (Capgemini) <cleytonurani@aneel.gov.br>
2018-01-23 17:07:28 -0200

    estilos padding

--- app2/App.js
+++ app2/App.js
@@ -4,13 +4,20 @@ import { Text, AppRegistry, View } from 'react-native';
 const Estilos = {
   estiloTexto: {
     fontSize: 30,
-    backgroundColor: 'red'
+    backgroundColor: 'red',
+    margin: 30,
+    shadowColor: '#000',
+    shadowOpacity: 1,
+    shadowOffset: {
+      width: 10,
+      height: 15
+    }
   }
 };
 
 const App = () => {
   return(
-    <Text style={Estilos.estiloTexto}>Olá mundo react</Text>
+    <Text style={Estilos.estiloTexto}>Frases do Dia</Text>
   )
 };
 
