5c3aa734e01c67d91d345bf66eb1cd65b699af0a
Cleyton Artur Soares Urani (Capgemini) <cleytonurani@aneel.gov.br>
2018-01-29 14:27:02 -0200

    aula 103

--- app3/App.js
+++ app3/App.js
@@ -7,35 +7,85 @@ import {
   Button
 } from 'react-native';
 
-class MeuComponente extends Component{
-  render(){
-    return (
-      <View>
-        <Text>{this.props.teste}</Text>
-      </View>
-    );
-  }
-}
-
 class app3 extends Component{
   constructor(props){
     super(props);
 
-    this.state = { texto: 'Texto teste2' };
-  };
+    this.state = { escolhaUsuario: '', escolhaComputador: ''};
+  }
 
-  alteraTexto(){
-    this.setState({ texto: 'Outra coisa' });
+  jokenpo(escolhaUsuario){
+    var numAleatorio = Math.floor(Math.random() *3);
+    var escolhaComputador = '';
+
+    switch(numAleatorio){
+      case 0: escolhaComputador = 'pedra'; break;
+      case 1: escolhaComputador = 'papel'; break;
+      case 2: escolhaComputador = 'tesoura'; break;
+    }
+
+    var resultado = '';
+
+    if(escolhaComputador == 'pedra'){
+      if(escolhaUsuario == 'pedra'){
+        resultado = 'Empate';
+      }
+      if(escolhaUsuario == 'tesoura')
+      {
+        resultado = 'Perdeu';
+      }
+      if(escolhaUsuario == 'papel')
+      {
+        resultado = 'Ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'papel'){
+      if(escolhaUsuario == 'papel')
+      {
+        resultado = 'Empate';
+      }
+      if(escolhaUsuario == 'pedra')
+      {
+        resultado = 'Perdeu';
+      }
+      if(escolhaUsuario == 'tesoura')
+      {
+        resultado = 'Ganhou';
+      }
+    }
+
+    if(escolhaComputador == 'tesoura'){
+      if(escolhaUsuario == 'tesoura')
+      {
+        resultado = 'Empate';
+      }
+      if(escolhaUsuario == 'papel')
+      {
+        resultado = 'Perdeu';
+      }
+      if(escolhaUsuario == 'pedra')
+      {
+        resultado = 'Ganhou';
+      }
+    }
+
+    this.setState({ 
+      escolhaUsuario : escolhaUsuario,
+      escolhaComputador : escolhaComputador,
+      resultado: resultado
+    });
   }
 
   render(){
     return (
       <View>
-        <Button
-          title='Botão'
-          onPress={() => {this.alteraTexto()}}
-        />
-        <MeuComponente teste={this.state.texto}></MeuComponente>
+        <Text>Escolha do Computador: {this.state.escolhaComputador}</Text>
+        <Text>Escolha do Usuário: {this.state.escolhaUsuario}</Text>
+        <Text>Resultado: {this.state.resultado}</Text>
+        <Button title='pedra' onPress={() => {this.jokenpo('pedra')}}/>
+        <Button title='papel' onPress={() => {this.jokenpo('papel')}}/>
+        <Button title='tesoura' onPress={() => {this.jokenpo('tesoura')}}/>
       </View>
     );
   }
