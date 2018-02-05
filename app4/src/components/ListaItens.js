00b84d162109e5870c97b60618cea3cfe6049090
Cleyton Artur Soares Urani (Capgemini) <cleytonurani@aneel.gov.br>
2018-02-05 09:48:46 -0200

    118

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import axios from 'axios';
 
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+    componentWillMount() {
+        //faus.com.br/recursos/c/dmairr/api/itens.html
+        axios
+            .get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+            .then(response => { console.log(response); })
+            .catch(() => { console.log('Erro ao passar os parametros.'); });
+    }
+
     render() {
         return (
             <View>
