b63d9b419dd084e42cd37278dc3b5c6db9ebb728
Cleyton Artur Soares Urani (Capgemini) <cleytonurani@aneel.gov.br>
2018-02-06 08:58:39 -0200

    123

--- app4/src/components/ListaItens.js
+++ app4/src/components/ListaItens.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import axios from 'axios';
 
 import Itens from './Itens';
 
 export default class ListaItens extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { listaItens: [] };
+    }
+
     componentWillMount() {
         //faus.com.br/recursos/c/dmairr/api/itens.html
         axios
             .get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-            .then(response => { console.log(response); })
+            .then(response => { this.setState({ listaItens: response.data }); })
             .catch(() => { console.log('Erro ao passar os parametros.'); });
     }
 
     render() {
         return (
-            <View>
-                <Itens />
-                <Itens />
-                <Itens />
-            </View>
+            <ScrollView style={{ backgroundColor: '#DDD' }}>
+                {this.state.listaItens.map(item => (<Itens key={item.titulo} item={item} />))}
+            </ScrollView>
         );
     }
 }
