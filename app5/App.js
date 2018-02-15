6599e672f52fb26c9513cd9787eabba742723146
Cleyton Artur Soares Urani (Capgemini) <cleytonurani@aneel.gov.br>
2018-02-15 09:15:19 -0200

    aula 133

--- app5/App.js
+++ app5/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import {
-  AppRegistry
+  AppRegistry, Navigator
 } from 'react-native';
 
 import CenaPrincipal from './src/components/CenaPrincipal';
+import CenaClientes from './src/components/CenaClientes';
 
 class app5 extends Component {
   render() {
     return (
-      <CenaPrincipal/>
+      <Navigator
+        InitialRoute={{id: 'a'}}
+        renderScene={(route, navigator) =>{
+          if (route.id === 'a'){
+            return(<CenaPrincipal />);
+          }
+          if (route.id === 'b'){
+            return(<CenaClientes />);
+          }
+        }}
+      />
     );
   }
 }
