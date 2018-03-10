50ce547cd3eddef5b2331acfd9ee496e4c6da55d
Cleyton Urani <cleytonnurani@gmail.com>
2018-03-10 17:00:29 -0300

    aula 149

---
+++ app6/App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+const logo = require('./src/imgs/logo.png');
+const btnjogar = require('./src/imgs/botao_jogar.png');
+const btnSobreJogo = require('./src/imgs/sobre_jogo.png');
+const btnOutrosJogos = require('./src/imgs/outros_jogos.png');
+
+export default class app6 extends Component {
+  render() {
+    return (
+      <View style={styles.cenaPrincipal}>
+        <View style={styles.apresentacaoJogo}>
+          <Image source={logo} />
+          <Image source={btnJogar} />
+        </View>
+
+        <View style={styles.rodape}>
+          <Image source={btnSobreJogo} />
+          <Image source={btnOutrosJogos} />
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  cenaPrincipal: {
+    flex: 1,
+    backgroundColor: '#61BD8C'
+  },
+  apresentacaoJogo:{
+    flex:10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  rodape:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  }
+});
