389802f6adbf078f886b2ff20a10c2885c346a1f
Cleyton <cleytonnurani@gmail.com>
2018-04-10 09:03:45 -0300

    aula170

--- app7/src/components/Operacao.js
+++ app7/src/components/Operacao.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { Picker, StyleSheet } from 'react-native';
 
-export default props => (
-    <View>
-        <Text>Operacao</Text>
-    </View>
-);
+export default class Operacao extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            operacao: ''
+        };
+    }
+
+    render() {
+        return (
+            <Picker 
+                style={styles.operacao}
+                selectedValue={this.state.operacao}
+                onValueChange={op => { this.setState({ operacao: op }); }}
+            >
+                <Picker.Item label='Soma' value='soma' />
+                <Picker.Item label='Subtração' value='subtracao' />
+            </Picker>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    operacao: {
+        marginTop: 15,
+        marginBotton: 15
+    }
+});
